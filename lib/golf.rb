d3b22975e410afd8da32897d62c97614d8f2bae2
John Hinrichs <jmhinric@gmail.com>
2014-05-27 19:21:56 -0400

    initial commit

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,38 @@
 class Golf
+  def .hole1(array)
+    array.reduce(1, :*) {|prod, num| num }
+  end
+
+  def .hole2
+
+  end
+  
+  def .hole3
+
+  end
+  
+  def .hole4
+
+  end
+  
+  def .hole5
+
+  end
+  
+  def .hole6
+
+  end
+  
+  def .hole7
+
+  end
+  
+  def .hole8
+
+  end
+  
+  def .hole9
+
+  end
+  
 end
