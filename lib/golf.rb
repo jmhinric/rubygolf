c6d940619de589bd08fe7e6d9f6c01e0a33694ca
Tom Blomfield <tomblomfield@gmail.com>
2014-05-27 19:33:22 -0400

    hole 2

--- lib/golf.rb
+++ lib/golf.rb
@@ -2,34 +2,35 @@ class Golf
   def self.hole1(array)
     array.reduce(1, :*) {|prod, num| num }
   end
-  def self.hole2
 
+  def self.hole2
+    ("a".."z").to_a
   end
-  
+
   def self.hole3 a
-  	(1..a).to_a.reduce(:*)
+  	(1..a).reduce(:*)
   end
-  
+
   def self.hole4
 
   end
-  
+
   def self.hole5
-  	
+
   end
-  
+
   def self.hole6
 
   end
-  
+
   def self.hole7
 
   end
-  
+
   def self.hole8
 
   end
-  
+
   def self.hole9
 
   end
